45bc7a6bf36911264e1669efb1f05bc6f47292fc
CelsonF <celson338540@gmail.com>
2021-07-28 22:16:52 -0300

    Add datas in app

--- vue/singlefile-components/App.vue
+++ vue/singlefile-components/App.vue
@@ -1,13 +1,25 @@
 <template>
-  
+   <h1> Hello {{name}} </h1>
 </template>
 
 <script>
 export default {
-
+ data() {
+     return {
+       name:'Celson'      
+     }
+ },
 }
 </script>
 
 <style>
+  
+  body {
+    background-color: #333;
+  }
 
+  h1{
+    color: #5f9ea0;
+    font-family:'Arial',sans-serif;
+  }
 </style>
