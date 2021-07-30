574133f8a0fdf6f3ec6c5efabe924731b39b298a
CelsonF <celson338540@gmail.com>
2021-07-29 22:59:50 -0300

    Create concepts using data-binding in vue js

---
+++ vue/data-binding/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <h1> Hello {{name}} </h1>
+   <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+export default {
+data: () => ({
+  name:'celson'  
+})
+}
+</script>
+
+<style>
+
+</style>
