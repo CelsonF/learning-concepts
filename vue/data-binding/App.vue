51e852b6e486dc4bc5953b246df2815b467bf428
CelsonF <celson338540@gmail.com>
2021-07-31 14:54:42 -0300

    finalized example two-way data binding

--- vue/data-binding/App.vue
+++ vue/data-binding/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1 v-bind:style="{textDecoration: decoration}" > Hello {{name}} </h1>
+    <h1 v-bind:style="{textDecoration: decoration}" > Hello {{name}} !</h1>
     <input type="text" v-model="name">
+    <br>
+    <a :href="link">Link do Curso!</a>
   </div>
 </template>
 
@@ -9,6 +11,7 @@
    export default {
     data: () => ({
       name:'celson',
+      link:'https://igorhalfeld.teachable.com/courses/treinamento-completo-e-gratuito-de-vue-js-3-do-iniciante-ao-avancado/lectures/29446833',
       decoration:'underline' 
     })
    }
