52e425bff0b761d4c249e8e6053d33cfe9fc5845
CelsonF <celson338540@gmail.com>
2021-08-04 23:45:50 -0300

    adding new example for events and methods

--- vue/Module_2/events-and-methods/App.vue
+++ vue/Module_2/events-and-methods/App.vue
@@ -1,47 +1,86 @@
 <template>
-   <div>
-     <h1> Minha lista de tarefas! </h1>
-      <button @click="() => showList = !showList">
-        Ver a lista
-      </button>
+  <div>
+    <h1>Minha lista de tarefas!</h1>
+    <button @click="handleShowHideList">Ver a lista</button>
 
-      <br>
-      <br>
-      
-      <input type="text" v-focus>
+    <br />
+    <br />
 
-      <ul v-if="showList">
-          <li
-          v-for="(task,index) in tasks"
+    <input type="text" 
+    v-focus 
+    v-model="currentTask"
+    @keyup.enter="addTask"
+    />
+
+    <ul v-if="showList">
+      <li v-for="(task, index) in tasks" 
           :key="`${task}-${index}`"
-          >
-           {{task.name}}
-             <button>
-              &times;
-             </button>
-          </li>
-      </ul>
+          :class="{
+            'line-through':task.isDone
+            }"
+          class="task-item"
+          @dblclick="complete(task)"
 
-      <p v-else> Lista de tarefas escondidas</p>
-   </div>  
+          >
+        {{ task.name }}
+        <button @click="remove(task)">&times;</button>
+      </li>
+    </ul>
+
+    <p v-else>Lista de tarefas escondidas</p>
+  </div>
 </template>
 
 <script>
 const focus = {
-    inserted:(el) => {
-      el.focus()
-    }
-}
+  inserted: (el) => {
+    el.focus();
+  },
+};
 
 export default {
-    directives: {
-        focus
+  directives: {
+    focus,
+  },
+  data: () => ({
+    currentTask:'',
+    showList: false,
+    tasks: [{ name: "Make curse", isDone: false }],
+  }),
+  methods: {
+    handleShowHideList() {
+      this.showList = !this.showList;
     },
-    data: () => ({
-       showList: false,
-       tasks: [
-           {name:'Make curse', isDone : false}
-       ]
-   })
-}
+    complete(task){
+      this.tasks = this.tasks.map(t => {
+        if (t.name === task.name) {
+          return { ...t,isDone: !t.isDone}
+        }
+        return {...t}
+      });
+    },
+    addTask() {
+      this.tasks.push({
+        name:this.currentTask,
+        isDone:false
+      })
+      this.currentTask = '';
+    }
+    ,
+    remove(task) {
+      this.tasks = this.tasks.filter(t => t.name !== task.name)
+    },
+  },
+};
 </script>
+
+<style scoped>
+.line-through {
+  text-decoration:line-through;
+}
+
+.task-item {
+  cursor: pointer;
+}
+
+</style>
