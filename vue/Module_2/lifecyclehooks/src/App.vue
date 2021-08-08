4eae8ba98abf19d15fdad5947ff27a72ca0005c5
CelsonF <celson338540@gmail.com>
2021-08-07 23:56:59 -0300

    add example for lifecycle hooks

---
+++ vue/Module_2/lifecyclehooks/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1> Life cycle hooks</h1>
+  </div>
+</template>
+
+<script>
+//Function hook normaly
+import {onMounted} from 'vue'
+
+export default {
+  setup() {
+   onMounted(() => {
+     console.log('Foi montado composition API');
+   });
+  }
+}
+
+// Example Options API
+// export default {
+//   mounted() {
+//     console.log('Foi montado options API')
+//   }
+// }
+</script>
+
+<style>
+
+</style>
